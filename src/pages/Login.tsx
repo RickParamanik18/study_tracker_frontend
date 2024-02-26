269dbdc024c8fb5f070c11d49b4de2c0cf4d7d00
Rick Paramanik <107174003+RickParamanik18@users.noreply.github.com>
2024-02-26 16:58:38 +0530

    Update Login.tsx :>

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,9 @@
 const Login = () => {
-    return <div>Login</div>;
+    const login = () => {
+        let creator: string | null = prompt("Enter your name");
+        localStorage.setItem("creator", creator ? creator : "sumana");
+    };
+    return <button onClick={login}>Enter</button>;
 };
 
 export default Login;
